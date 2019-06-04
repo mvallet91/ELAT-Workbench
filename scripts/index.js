f0038a4309e41bab3bf35e012d19d3110680224b
Manuel Valle <mvallet91@gmail.com>
2019-06-04 15:35:49 +0200

    Video arc chart

--- scripts/index.js
+++ scripts/index.js
@@ -3555,3 +3555,164 @@ function videoTransitions() {
         }
     });
 }
+
+
+function drawVideoArc(){
+
+    let nodeData = "scripts/videoData.json";
+
+    // let nodeData = JSON.parse('{"nodes":[{"name":"Video 1","n":5,"grp":1,"id":"v1"},{"name":"Video 2","n":5,"grp":2,"id":"v2"},{"name":"Video 3","n":5,"grp":3,"id":"v3"},{"name":"Video 4","n":5,"grp":4,"id":"v4"},{"name":"Video 5","n":5,"grp":4,"id":"v5"}],"links":[{"source":"v1","target":"v2","value":1},{"source":"v2","target":"v3","value":1},{"source":"v3","target":"v4","value":1},{"source":"v4","target":"v5","value":1}],"attributes":{}} ');
+
+    let margin = {top: 0, right: 30, bottom: 50, left: 60},
+        width = 1050 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+    let svg = d3.select("#videoArc")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+// Read dummy data
+//     d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_researcherNetwork.json", function( data) {
+    d3.json(nodeData, function( data) {
+
+        // List of node names
+        let allNodes = data.nodes.map(function(d){return d.name})
+
+        // List of groups
+        let allGroups = data.nodes.map(function(d){return d.grp})
+        allGroups = [...new Set(allGroups)]
+
+        // A color scale for groups:
+        let color = d3.scaleOrdinal()
+            .domain(allGroups)
+            .range(d3.schemeSet3);
+
+        // A linear scale for node size
+        let size = d3.scaleLinear()
+            .domain([1,10])
+            .range([2,10]);
+
+        // A linear scale to position the nodes on the X axis
+        let x = d3.scalePoint()
+            .range([0, width])
+            .domain(allNodes)
+
+        // In my input data, links are provided between nodes -id-, NOT between node names.
+        // So I have to do a link between this id and the name
+        let idToNode = {};
+        data.nodes.forEach(function (n) {
+            idToNode[n.id] = n;
+        });
+
+        // let tooltip2 = svg
+        //     .selectAll('myLinksInfo')
+        //     .append("div")
+        //     .data(data.nodes)
+        //     .style("position", "absolute")
+        //     .style("z-index", "10")
+        //     .style("visibility", "hidden")
+        //     .text(function(d){ return (d.name)});
+
+        // Add the links
+        let links = svg
+            .selectAll('mylinks')
+            .data(data.links)
+            .enter()
+            .append('path')
+            .attr('d', function (d) {
+                start = x(idToNode[d.source].name)    // X position of start node on the X axis
+                end = x(idToNode[d.target].name)      // X position of end node
+                return ['M', start, height-30,    // the arc starts at the coordinate x=start, y=height-30 (where the starting node is)
+                    'A',                            // This means we're gonna build an elliptical arc
+                    (start - end)/2, ',',    // Next 2 lines are the coordinates of the inflexion point. Height of this point is proportional with start - end distance
+                    (start - end)/2, 0, 0, ',',
+                    start < end ? 1 : 0, end, ',', height-30] // We always want the arc on top. So if end is before start, putting 0 here turn the arc upside down.
+                    .join(' ');
+            })
+            .style("fill", "none")
+            .attr("stroke", "grey")
+            .style("stroke-width", function(d){ return 10*(d.value)});
+
+        // Add the circle for the nodes
+        let nodes = svg
+            .selectAll("mynodes")
+            .data(data.nodes.sort(function(a,b) { return +b.n - +a.n }))
+            .enter()
+            .append("circle")
+            .attr("cx", function(d){ return(x(d.name))})
+            .attr("cy", height-30)
+            .attr("r", function(d){ return(size(d.n))})
+            .style("fill", function(d){ return color(d.grp)})
+            .attr("stroke", "white")
+
+        // And give them a label
+        let labels = svg
+            .selectAll("mylabels")
+            .data(data.nodes)
+            .enter()
+            .append("text")
+            .attr("x", 0)
+            .attr("y", 0)
+            .text(function(d){ return(d.name)} )
+            .style("text-anchor", "end")
+            .attr("transform", function(d){ return( "translate(" + (x(d.name)) + "," + (height-15) + ")rotate(-45)")})
+            .style("font-size", 10)
+
+
+        let tooltip = d3.select("body")
+            .append("div")
+            .data(data.nodes)
+            .text(function(d){ return d.info })
+            .style("position", "absolute")
+            .style("z-index", "10")
+            .style("visibility", "hidden");
+
+        // Add the highlighting functionality
+        nodes
+            .on('mouseover', function (d) {
+                // Highlight the nodes: every node is green except of him
+                nodes
+                    .style('opacity', .2);
+                d3.select(this)
+                    .style('opacity', 1);
+                // Highlight the connections
+                links
+                    .style('stroke', function (link_d) { return link_d.source === d.id || link_d.target === d.id ? color(d.grp) : '#b8b8b8';})
+                    .style('stroke-opacity', function (link_d) { return link_d.source === d.id || link_d.target === d.id ? 1 : .2;})
+                    .style('stroke-width', function (link_d) { return link_d.source === d.id || link_d.target === d.id ? 10*(d.value) : 5;});
+                    // .style("stroke-width", function(d){ return 10*(d.value)})
+                labels
+                    .style("font-size", function(label_d){ return label_d.name === d.name ? 16 : 2 } )
+                    .attr("y", function(label_d){ return label_d.name === d.name ? 10 : 0 } );
+                tooltip
+                    .style("top", (event.pageY-390)+"px")
+                    .style("left",(event.pageX-100)+"px")
+                    .html(d.info)
+                    // .attr("data-html", "true")
+                    .style("visibility", "visible");
+
+            })
+            // .on('mousemove', function (d) {
+            //     tooltip
+            //         .style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
+            //
+            // })
+            .on('mouseout', function (d) {
+                nodes.style('opacity', 1);
+                links
+                    .style('stroke', 'grey')
+                    .style('stroke-opacity', .8)
+                    .style("stroke-width", function(d){ return 10*(d.value)});
+                labels
+                    .style("font-size", 6 );
+                tooltip
+                    .style("visibility", "hidden");
+
+            })
+    })
+}

7f708b1a7679276fbd9c5291e025d436473a141c
mvallet91 <mvallet91@gmail.com>
2019-10-08 17:50:42 +0200

    Create hidden copy

--- test/browser.test.js
+++ test/browser.test.js
@@ -9,14 +9,20 @@ describe('renders without crashing', () => {
         const height = 1704;
         let args = [];
         args.push('--no-sandbox');
-        let browser =  await puppeteer.launch({ headless: false, ignoreDefaultArgs: ['--disable-extensions'], args});
+        let browser =  await puppeteer.launch({ headless: true, ignoreDefaultArgs: ['--disable-extensions'], args});
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98');
-        await page.setViewport({ width, height })
+        await page.setViewport({ width, height });
 
-        await page.goto('https://mvallet91.github.io/hidden_workbench/');
+        await page.goto('https://mvallet91.github.io/hidden_workbench/', {waitUntil: 'domcontentloaded'});
+        // await page.waitForNavigation({ waitUntil: 'networkidle2' });
+        // await page.waitForSelector('#buttons > button:nth-child(5)', { visible: true });
+
+        await page.screenshot({path: 'img/screenshot.png'});
+
+        // await page.click('#uploadFilesButton');
         expect(page).not.toBeNull();
 
         // browser.close();
-    }, 10000 );
+    }, 15000 );
 });
